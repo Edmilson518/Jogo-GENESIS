bd2a897d75abae337f9b31c4d31aeb9991817cbe
Edmilson518 <oreia_157_122@outlook.com>
2022-03-30 19:35:11 -0300

    jogo genesis

---
+++ script.js
@@ -0,0 +1,103 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 = verde
+//1 = vermelho
+//2 = amarelo
+//3 = azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+//cria ordem para aletoria de cores
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightcolor(elementColor, Number(i) + 1);
+    }  
+}
+// ACENDE A PROXIMA COR
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+       element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
+//checa se os botoes clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert('Pontução: ${score}\nVocê acertou! Iniciando próximo nivel!');
+        nextlevel();
+    }
+}
+
+//função para clique do usúario
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    },250);   
+}
+
+//Função que retorna a cor
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if(color == 2) {
+        return yellow;
+    } else if(color == 3) {
+        return blue;
+    }
+}
+
+//função para proximo nivel do jogo
+let nextlevel = () => {
+    score++;
+    shuffleOrder();
+
+}
+
+//função para game over
+let gameOver = () =>{
+    alert('pontuação: ${score}!\nVocê perdeu o jogo!\nclique em ok para iniciar um novo jogo');
+    order = [];
+    clickedOrder = [];
+
+    playgame();
+}
+
+let playgame = () => {
+    alert('bem vindo ao genesis! iniciando o jogo!');
+    score = 0;
+
+    nextlevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playgame();
